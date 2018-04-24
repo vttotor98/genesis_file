6b68d40c337b54e79de1668e7e704d26db3cd88c
vttotor98 <33938785+vttotor98@users.noreply.github.com>
2018-04-24 17:28:05 +0200

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#Demande du nom du noeud
+echo "Comment vous voulez appeller ce noeud ?"
+read node_name
+
+#initialisation du noeud
+geth --identity node_name init ~/blockchain/genesis_folder/firstGenesis.json --datadir ~/blockchain/.ethereum_private/AgaetisChain
