e9b3bae025d34d0e87399dc0b338bf07748bf4cd
vttotor98 <33938785+vttotor98@users.noreply.github.com>
2018-04-25 11:29:58 +0200

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#Author : Victor TIREBAQUE for Agaetis Clermont-Ferrand
+
+#Installation des dépôts et paquest nécessaires
+sudo add-apt-repository -y ppa:ethereum/ethereum
+sudo add-apt-repository -y ppa:ethereum/ethereum-dev
+sudo apt-get update
+sudo apt-get install -y build-essential golang software-properties-common ethereum eth
+
+#Finalisation de l'installation et initialistation de la blockchain
+./ether.sh
