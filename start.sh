19537c413deb167b003db587acaf3da227640f86
vttotor98 <33938785+vttotor98@users.noreply.github.com>
2018-04-25 11:19:34 +0200

    Delete start.sh

--- start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#Author : Victor TIREBAQUE for Agaetis Clermont-Ferrand
-
-#Demande du nom du noeud
-echo "Comment vous voulez appeller ce noeud ?"
-read node_name
-
-#Initialisation du noeud
-geth --identity node_name init ~/blockchain/genesis_folder/firstGenesis.json --datadir ~/blockchain/.ethereum_private/AgaetisChain
