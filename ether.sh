dd7e9b3931b7f8232e9b93170b8dc342f67a2534
vttotor98 <33938785+vttotor98@users.noreply.github.com>
2018-04-25 18:00:27 +0200

    Update ether.sh

--- ether.sh
+++ ether.sh
@@ -2,10 +2,10 @@
 #Author : Victor TIREBAQUE for Agaetis Clermont-Ferrand
 
 #Création du dossier du fichier json 
-mkdir -p ~/blockchain/genesis_folder
-mv genesis.json ~/blockchain/genesis_folder/
+mkdir -p /blockchain/genesis_folder
+mv genesis.json /blockchain/genesis_folder/
 #Lors de l'exécution de la commande attention à l'id de la blockchain
-geth --datadir ~/blockchain/.ethereum_private init ~/blockchain/genesis_folder/firstGenesis.json 
+geth --datadir /blockchain/.ethereum_private init ~/blockchain/genesis_folder/firstGenesis.json 
 
 #Démarage du premier noeuds, vérifier les paramètres 
 echo "Comment vous voulez appeller ce noeud ?"
