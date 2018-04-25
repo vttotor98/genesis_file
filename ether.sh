780aef7a6db80419f995e4e3678433589d8d2f03
vttotor98 <33938785+vttotor98@users.noreply.github.com>
2018-04-25 17:59:30 +0200

    Update ether.sh

--- ether.sh
+++ ether.sh
@@ -12,4 +12,3 @@ echo "Comment vous voulez appeller ce noeud ?"
 read node_name
 geth --identity $node_name init ~/blockchain/genesis_folder/firstGenesis.json --datadir ~/blockchain/.ethereum_private/ACPrivateChain
 echo “pour quitter faire Ctrl + C”
-geth --datadir ~/blockchain/.ethereum_private/ACPrivateChain --networkid 988 &
