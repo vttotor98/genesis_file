9963d1eee02db18503c0c0551ef5cfabe6108b03
vttotor98 <33938785+vttotor98@users.noreply.github.com>
2018-04-25 11:28:54 +0200

    Create install_docker.sh

---
+++ install_docker.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#Author : Victor TIREBAQUE for Agaetis Clermont-Ferrand
+
+#Installation des dépôts et paquest nécessaires
+add-apt-repository -y ppa:ethereum/ethereum
+add-apt-repository -y ppa:ethereum/ethereum-dev
+apt-get update
+apt-get install -y build-essential golang ethereum eth git
+
+#Finalisation de l'installation et initialistation de la blockchain
+./ether.sh
