7fcdeb085b474c4c93e8faba9bd8347f916ae5fa
vttotor98 <33938785+vttotor98@users.noreply.github.com>
2018-04-26 16:20:05 +0200

    Create start_blch.sh

---
+++ start_blch.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#Author Victor TIREBAQUE for Agaetis Clermont-Ferrand
+
+#Démarage du premier noeuds, vérifier les paramètres
+$file=./.param_blch
+#Est-ce que le fichier de paramètre existe, et est-il vide
+if [ ! -s $file] then
+  #Comme il n'existe pas de fichier de paramètrage 
+  #Nous le créons en demandant les paramètres à l'utilisateur
+  echo "Comment vous voulez appeller ce noeud ?"
+  read node_name
+  echo "Quel sera l'idnetwork ?"
+  read idnet
+
+  #Création du fichier .param_blch
+  touch ./.param_blch
+  chmod +x .param_blch
+  echo $node_name >.param_blch
+  echo $idnet >>.param_blch
+else
+  #Comme il existe nous récupérons les données
+  declare -a tab
+  i=0
+  while IFS='' read -r line || [[ -n "$line" ]]; do
+    tab[i]=$line
+    i++
+  done < .param_blch
+  
+  $node_name=tab[0]
+  $idnet=tab[1]
+  
+fi
+
+geth --identity $node_name init /blockchain/genesis_folder/genesis.json --datadir /blockchain/.ethereum_private/AgaetisChain
+geth --datadir ~/blockchain/.ethereum_private/AgaetisChain --networkid $idnet &
