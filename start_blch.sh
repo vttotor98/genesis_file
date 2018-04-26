eb9e2f247bb4ecc5b74104bc053fcc87bb56a2e9
vttotor98 <33938785+vttotor98@users.noreply.github.com>
2018-04-26 17:52:33 +0200

    Update start_blch.sh

--- start_blch.sh
+++ start_blch.sh
@@ -2,19 +2,19 @@
 #Author Victor TIREBAQUE for Agaetis Clermont-Ferrand
 
 #Démarage du premier noeuds, vérifier les paramètres
-$file=.param_blch
+fichier=./.param_blch
 #Est-ce que le fichier de paramètre existe, et est-il vide
-if [ ! -s $file]; then
-  #Comme il n'existe pas de fichier de paramètrage 
-  #Nous le créons en demandant les paramètres à l'utilisateur
+if [ ! -s $fichier ]; then
+  #Comme il n'existe pas de fichier de paramètrage
+  #Nous le créons en demandant les paramètres ▒|  l'utilisateur
   echo "Comment vous voulez appeller ce noeud ?"
   read node_name
   echo "Quel sera l'idnetwork ?"
   read idnet
 
   #Création du fichier .param_blch
-  touch ./.param_blch
-  chmod +x .param_blch
+  touch $fichier
+  chmod 755 .param_blch
   echo $node_name >.param_blch
   echo $idnet >>.param_blch
 else
@@ -23,18 +23,18 @@ else
   i=0
   while IFS='' read -r line || [[ -n "$line" ]]; do
     tab[i]=$line
-    i++
+    i=`expr $i + 1`
   done < .param_blch
-  
-  $node_name=tab[0]
-  $idnet=tab[1]
-  
+
+  node_name=tab[0]
+  idnet=tab[1]
+
 fi
 
 geth --identity $node_name init /blockchain/genesis_folder/genesis.json --datadir /blockchain/.ethereum_private/AgaetisChain
 
 #Si nous voulons placer en paramètres des options ou commande, c'est possible grâce au if
-if [ $# -gt 1 ]; then
+if [ $# -gt 0 ]; then
   geth --datadir ~/blockchain/.ethereum_private/AgaetisChain --networkid $idnet $@
 else
   geth --datadir ~/blockchain/.ethereum_private/AgaetisChain --networkid $idnet &
