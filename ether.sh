360dfe5ce19f7d3a6c588dc0cf2d848a0c7f6316
vttotor98 <33938785+vttotor98@users.noreply.github.com>
2018-04-26 10:55:41 +0200

    Update ether.sh

--- ether.sh
+++ ether.sh
@@ -3,6 +3,7 @@
 
 #Création du dossier du fichier json 
 mkdir -p /blockchain/genesis_folder
+pwd
 mv ./genesis.json /blockchain/genesis_folder/
 #Lors de l'exécution de la commande attention à l'id de la blockchain
 geth --datadir /blockchain/.ethereum_private init /blockchain/genesis_folder/firstGenesis.json 
@@ -17,3 +18,4 @@ cd /blockchain
 echo "geth --identity "$node_name" init /blockchain/genesis_folder/firstGenesis.json --datadir /blockchain/.ethereum_private/ACPrivateChain" >start.sh
 echo "geth --datadir ~/blockchain/.ethereum_private/ACPrivateChain --networkid 988" >>start.sh 
 
+exit
