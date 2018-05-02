9d5fedbf9757c4121871a5341ae48302f949df14
vttotor98 <33938785+vttotor98@users.noreply.github.com>
2018-05-02 16:41:06 +0200

    Update install_docker.sh

--- install_docker.sh
+++ install_docker.sh
@@ -5,7 +5,7 @@
 add-apt-repository -y ppa:ethereum/ethereum
 add-apt-repository -y ppa:ethereum/ethereum-dev
 apt-get update
-apt-get install -y build-essential golang ethereum eth
+apt-get install -y build-essential golang ethereum eth geth
 
 #Finalisation de l'installation et initialistation de la blockchain
 genesis_files/ether.sh
