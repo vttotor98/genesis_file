8d899d7e1f48c3a6c6fe5d2351050ca482f74918
vttotor98 <33938785+vttotor98@users.noreply.github.com>
2018-05-03 11:19:17 +0200

    Update install_docker.sh

--- install_docker.sh
+++ install_docker.sh
@@ -6,6 +6,7 @@ add-apt-repository -y ppa:ethereum/ethereum
 add-apt-repository -y ppa:ethereum/ethereum-dev
 apt-get update
 apt-get install -y build-essential
+apt-get install -y software-properties-common
 apt-get install -y golang 
 apt-get install -y ethereum 
 apt-get install -y geth
