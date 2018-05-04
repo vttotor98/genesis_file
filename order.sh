40dcf031f90108cc81bd8a49a8d2d71bfc39ce49
vttotor98 <33938785+vttotor98@users.noreply.github.com>
2018-05-04 14:36:05 +0200

    Update order.sh

--- order.sh
+++ order.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 #Author Victor TIREBAQUE for Agaetis
 
+csl="console"
 #Exécution d'une requête passée en argument
-geth --exec $1 attach ~/blockchain/.ethereum_private/AgaetisChain/geth.ipc
+if [ $1 = $csl ]; then
+  geth attach ~/blockchain/.ethereum_private/AgaetisChain/geth.ipc $1
+else
+  geth --exec $1 attach ~/blockchain/.ethereum_private/AgaetisChain/geth.ipc
+fi
