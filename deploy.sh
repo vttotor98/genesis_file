6e68a78437cf9c251c2f854d1bdb52c192c180c0
vttotor98 <33938785+vttotor98@users.noreply.github.com>
2018-05-16 10:16:09 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 #Author Victor TIREBAQUE for Agaetis
 
-
-
 if [ $1 = "help" ]; then 
-  echo "Il faut mettre le 
+  echo "Il faut mettre le nom du fichier du contrat en argument, et vous obtenez le fichier compiler dans un fichier .js"
 else
   a=$1
   a=`echo "$a" | tr '[:upper:]' '[:lower:]'`
