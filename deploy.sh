424e602e2290cf9d83808129bb5f1d27ed7c3051
vttotor98 <33938785+vttotor98@users.noreply.github.com>
2018-05-16 10:35:29 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,8 +4,14 @@
 if [ $1 = "help" ]; then 
   echo "Il faut mettre le nom du fichier du contrat en argument, et vous obtenez le fichier compiler dans un fichier .js"
 else
-  a=$1
-  a=`echo "$a" | tr '[:upper:]' '[:lower:]'`
-  echo $a
+  file_name=$1
+  file_name=`echo "$file_name" | tr '[:upper:]' '[:lower:]'`
+  file_name=`echo ${file_name%%.*}`
+  output=$file_name
+  file_name="$file_name.js"
+#  echo $file_name
 
+  output="$outputOutput"
+  echo "var $output=`solc --optimize --combined-json abi,bin,interface $1`" > $file_name
+  
 fi
