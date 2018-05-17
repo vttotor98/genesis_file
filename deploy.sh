8adcc1d616865b69e117fe19bc98a1d015cb9acd
vttotor98 <33938785+vttotor98@users.noreply.github.com>
2018-05-17 10:41:40 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -12,16 +12,28 @@ else
   output=$fct"Output"
   file_name=$fch".js"
   
+  #Compilation du contract
   solc --optimize --gas --abi --bin $1 >$file_name
   
+  #Extraction du code binaire et de l'abi
   bin=`sed -n '12'p $file_name`
   abi=`sed -n '14'p $file_name`
+  #Abi call & Bin call variables
   abic=$fct"Abi"
+  binc=$fct"BinCode"
   
   reqst="var $abic = $output.contracts['$1:$fct'].abi"
-  reqst=$reqst'"'$abi'"'
-  
-#  ./order.sh $reqst
+# reqst="var $abic = $abi" 
+# reqst=$reqst'"'$abi'"'
+# ./order.sh $reqst
+
+  fncontr=$fct"Contract = eth.contract(JSON.parse("$abic"))"
+# ./order.sh $fncontr
+
+  rqstbin='var $binc = "0x" + $output.contracts['$1:$fct'].bin'
+# rqstbin='var $binc = $bin'
+# rqstbin=$rqstbin'"'$bin'"'
+# ./order.sh $rqstbin
   
   var account = web3.eth.accounts[0];                                                          
   var addContract = web3.eth.contract(<content/of/ABI>);                                         
