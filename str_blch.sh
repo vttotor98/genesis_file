c3c23b1d81749803bf165ee1bb52ce3c6917ee70
vttotor98 <victor.tirebaque.988@gmail.com>
2018-05-24 16:35:41 +0200

    modification des variables path et de touch

--- str_blch.sh
+++ str_blch.sh
@@ -1,43 +1,48 @@
 #!/bin/bash
 #Author Victor TIREBAQUE for Agaetis Clermont-Ferrand
 
+home="~/blockchain/"
+doss=$home".ethereum_private/ACPrivateChain/"
+fichier=$doss".param_blch"
+gl=$doss"geth.log"
+
+
 #Démarage du premier noeuds, vérifier les paramètres
-doss=~/blockchain/.ethereum_private/ACPrivateChain/
-fichier=$doss.param_blch
 #Est-ce que le fichier de paramètre existe, et est-il vide
 if [ ! -s $fichier ]; then
-  #Comme il n'existe pas de fichier de paramètrage
-  #Nous le créons en demandant les paramètres à l'utilisateur
-  echo "Comment vous voulez appeller ce noeud ?"
-  read node_name
-  echo "Quel sera l'idnetwork ?"
-  read idnet
-
-  #Création du fichier .param_blch
-  touch $fichier
-  chmod 755 .param_blch
-  echo $node_name >.param_blch
-  echo $idnet >>.param_blch
+	#Comme il n'existe pas de fichier de paramètrage
+	#Nous le créons en demandant les paramètres à l'utilisateur
+	echo "Comment vous voulez appeller ce noeud ?"
+	read node_name
+	echo "Quel sera l'idnetwork ?"
+	read idnet
+	
+	#Création du fichier $fichier
+	touch "$fichier"
+	chmod 755 $fichier
+	echo $node_name >$fichier
+	echo $idnet >>$fichier
 else
-  #Comme il existe nous récupérons les données
-  declare -a tab
-  i=0
-  #Lecture dans le fichier .param_blch
-  while IFS='' read -r line || [[ -n "$line" ]]; do
-    tab[i]=$line
-    i=`expr $i + 1`
-  done < .param_blch
+	#Comme il existe nous récupérons les données
+	declare -a tab
+	i=0
 
-  node_name=${tab[0]}
-  idnet=${tab[1]}
+	#Lecture dans le fichier $fichier
+	while IFS='' read -r line || [[ -n "$line" ]]; do
+		tab[i]=$line
+		i=`expr $i + 1`
+	done < $fichier
+	
+	node_name=${tab[0]}
+	idnet=${tab[1]}
 
 fi
 
 
 #Initialisation de démarrage et redirection du fichier nohup.out dans geth.log
 geth --identity $node_name init ~/blockchain/genesis_folder/genesis.json --datadir ~/blockchain/.ethereum_private/ACPrivateChain
-nohup geth --datadir ~/blockchain/.ethereum_private/ACPrivateChain --networkid $idnet --nodiscover &>>/geth.log &
+nohup geth --datadir ~/blockchain/.ethereum_private/ACPrivateChain --networkid $idnet --nodiscover &>>$gl &
 
 
 #Don des droits en lecture
-chmod +r geth.log
+chmod +r $gl
