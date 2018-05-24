3a75e90c4054527983fb20e95ddd94d6733ecfcf
vttotor98 <victor.tirebaque.988@gmail.com>
2018-05-24 18:25:33 +0200

    test d'un truc

--- ether.sh
+++ ether.sh
@@ -9,17 +9,33 @@ sousdoss=$doss"functions/"
 #Création du dossier du fichier json 
 mkdir -p $gf
 
+tree 
+read
+
 cp $doss"genesis.json" $home"genesis_folder/"
+
+tree
+read
 
 #Copie du dossier contenant quelques fonctions
 cp $sousdoss $home
+
+tree
+read
 
 #Lors de l'exécution de la commande attention à l'id de la blockchain
 geth --datadir /blockchain/.ethereum_private init /blockchain/genesis_folder/genesis.json 
 
 #Déplacement des fichiers d'exécution dans blockchain
+
+tree
+read
+
 cp $doss"order.sh" $home
 cp $doss"start_blch.sh" $home
+
+tree
+read
 
 echo "Exécuter start_blch.sh pour démarrer la blockchain dans $home"
 echo "Vous pouvez supprimer le dossier genesis_files"
