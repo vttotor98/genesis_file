1a63a88a8cd23698d50f1d7561544c847a6d26f2
vttotor98 <victor.tirebaque.988@gmail.com>
2018-05-24 15:32:53 +0200

    Remplacement de tous les mv en cp dans ether.sh

--- ether.sh
+++ ether.sh
@@ -3,18 +3,18 @@
 
 #Création du dossier du fichier json 
 mkdir -p /blockchain/genesis_folder
-mv /genesis_files/genesis.json /blockchain/genesis_folder/
+cp /genesis_files/genesis.json /blockchain/genesis_folder/
 
 #Création du dossier avec quelques fonctions
 mkdir /blockchain/functions
-mv ./functions.js /blockchain/functions
+cp ./functions.js /blockchain/functions
 
 #Lors de l'exécution de la commande attention à l'id de la blockchain
 geth --datadir /blockchain/.ethereum_private init /blockchain/genesis_folder/genesis.json 
 
 #Déplacement des fichiers d'exécution dans blockchain
-mv /genesis_files/order.sh /blockchain
-mv /genesis_files/start_blch.sh /blockchain
+cp /genesis_files/order.sh /blockchain
+cp /genesis_files/start_blch.sh /blockchain
 
 chmod +x /blockchain/*.sh
 
