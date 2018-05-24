11758680f461ce8b7e301ac62b0d0e1dacfbd8bb
vttotor98 <victor.tirebaque.988@gmail.com>
2018-05-24 17:16:38 +0200

    modification pour une portabilité

--- install.sh
+++ install.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
 #Author : Victor TIREBAQUE for Agaetis Clermont-Ferrand
 
+if [ $1 = "ubuntu" ] ; then
+	        os="sudo"
+		else
+			        os=""
+				fi
+
 #Installation des dépôts et paquest nécessaires
-add-apt-repository -y ppa:ethereum/ethereum
-add-apt-repository -y ppa:ethereum/ethereum-dev
-apt-get update
-apt-get install -y build-essential
-apt-get install -y software-properties-common
-apt-get install -y golang 
-apt-get install -y ethereum 
-apt-get install -y geth
+$os add-apt-repository -y ppa:ethereum/ethereum
+$os add-apt-repository -y ppa:ethereum/ethereum-dev
+$os apt-get update
+$os apt-get install -y build-essential
+$os apt-get install -y software-properties-common
+$os apt-get install -y golang
+$os apt-get install -y ethereum
+$os apt-get install -y geth
 
 #Finalisation de l'installation et initialistation de la blockchain
 genesis_files/ether.sh
+
