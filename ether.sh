d1fd226ab60f5aa9e01717ba13880d19724a491d
vttotor98 <victor.tirebaque.988@gmail.com>
2018-05-24 17:34:52 +0200

    problème de copie des fichiers 1ère tentative

--- ether.sh
+++ ether.sh
@@ -7,18 +7,18 @@ gf=$home"genesis_folder/"
 #Création du dossier du fichier json 
 mkdir -p $gf
 
-cp ./genesis.json $home"genesis_folder/"
+cp genesis.json $home"genesis_folder/"
 
 #Création du dossier avec quelques fonctions
 mkdir $home"functions"
-cp ./functions/functions.js $home"functions"
+cp functions/functions.js $home"functions"
 
 #Lors de l'exécution de la commande attention à l'id de la blockchain
 geth --datadir /blockchain/.ethereum_private init /blockchain/genesis_folder/genesis.json 
 
 #Déplacement des fichiers d'exécution dans blockchain
-cp ./order.sh $home
-cp ./start_blch.sh $home
+cp order.sh $home
+cp start_blch.sh $home
 
 echo "Exécuter start_blch.sh pour démarrer la blockchain dans $home"
 echo "Vous pouvez supprimer le dossier genesis_files"
