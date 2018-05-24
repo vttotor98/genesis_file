4dc084763f93e548e3eb631709734f5e52e4fbad
vttotor98 <victor.tirebaque.988@gmail.com>
2018-05-24 17:24:42 +0200

    modification des path pour que cela fonctionne

--- ether.sh
+++ ether.sh
@@ -1,20 +1,24 @@
 #!/bin/bash
 #Author : Victor TIREBAQUE for Agaetis Clermont-Ferrand
 
+home=~/blockchain/
+gf=$home"genesis_folder/"
+
 #Création du dossier du fichier json 
-mkdir -p /blockchain/genesis_folder
-cp /genesis_files/genesis.json /blockchain/genesis_folder/
+mkdir -p $gf
+
+cp ./genesis.json $home"genesis_folder/"
 
 #Création du dossier avec quelques fonctions
-mkdir /blockchain/functions
-cp ./functions.js /blockchain/functions
+mkdir $home"functions"
+cp ./functions/functions.js $home"functions"
 
 #Lors de l'exécution de la commande attention à l'id de la blockchain
 geth --datadir /blockchain/.ethereum_private init /blockchain/genesis_folder/genesis.json 
 
 #Déplacement des fichiers d'exécution dans blockchain
-cp /genesis_files/order.sh /blockchain
-cp /genesis_files/start_blch.sh /blockchain
+cp ./order.sh $home
+cp ./start_blch.sh $home
 
-echo "Exécuter start_blch.sh pour démarrer la blockchain dans blockchain"
+echo "Exécuter start_blch.sh pour démarrer la blockchain dans $home"
 echo "Vous pouvez supprimer le dossier genesis_files"
