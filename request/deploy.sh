465a1ef63dd55d0c5644b5fd7c62c79e29440bec
vttotor98 <victor.tirebaque.988@gmail.com>
2018-05-25 15:00:22 +0200

    réécriture des commentaires pour une meilleure lisibilité

--- request/deploy.sh
+++ request/deploy.sh
@@ -1,27 +1,28 @@
 #!/bin/bash
 #Author Victor TIREBAQUE for Agaetis
 
-home=$HOME
-depl="/blockchain/functions/deploy.js"
+home=$HOME/blockchain
+doss=$home/functions
+depl=$doss/deploy.js
 
-#Fonction qui permet d'écrire le contrat
+#Fonction qui permet d'écrire le contrat dans un fichier
 function ins_depl() {
 	echo $1 >> $depl
-	echo $1
+	#echo $1
 }
 
+#Si l'argument est vide ou est "help"
 if [ -z $1 ] || [ $1 = "help" ] ; then 
 
 	#Aide
 	echo "Il faut mettre le nom du fichier du contrat en argument, et vous obtenez le fichier compilé dans un fichier .js"
 
 else
-  
+  	#L'argument n'est pas vide ni est "help"
 	#La chaîne de caractère qui est le nom du fichier monFichier.sol devient monfichier.js
 	fct=$1
 	fct=`echo ${fct%%.*}`
 	fch=`echo "$fct" | tr '[:upper:]' '[:lower:]'`
-	output=$fct"Output"
 	file_name=$fch".js"
   
 	#Compilation du contract
@@ -31,39 +32,37 @@ else
 	bin=`sed -n '12'p $file_name`
 	abi=`sed -n '14'p $file_name`
 
-	#Abi call & Bin call variables
-	abic=$fct"Abi"
-	binc=$fct"BinCode"
-
-
 	#Choix du compte à partir duquel nous déployons le contrat
+	$doss/checkAllBalance.sh
+		
+	echo -n "Quel est le compte à partir duquel vous voulez déployer le contrat ? "
+	read account
+	echo 
+	echo -n "Veuillez entrer le mot de passe :"
+	read -s password
+	echo
+
+	#Dépoiement du contrat, nous insérons chaque ligne dans le fichier deploy.js
+	echo "" >$depl
+	ins_depl "var myAccount = web3.eth.accounts["$account"];"
+	ins_depl "web3.personal.unlockAccount(myAccount,'$password',10000);"
+	ins_depl "var addContract = web3.eth.contract("$abi");"                                       
+	ins_depl 'var binary = "0x'$bin'";'                                                          
+	ins_depl "var estimatedGas = web3.eth.estimateGas({data: binary});"                                       
+	ins_depl "var addContractInstance = addContract.new({data: binary, from: myAccount, gas: estimatedGas})"
+
+	#Purge du fichier log avant l'utiliser
 	lof='log_deploy.log'
 	echo "" >$lof
-	./functions/checkAllBalance.sh
 
-	#Si le fichier est vide, l'exécution du script sera possible
-	test=`tail -n 2 $lof | sed -n '1'p | cut -d " " -f 1`
+	#Déploiement du contrat dans la blockchain
+	~/order.sh "loadScript('"$home$depl"');" >>$lof
+
 	
+	#Si le fichier log possède l'erreur
+	test=`tail -n 2 $lof 2>/dev/null | sed -n '1'p | cut -d " " -f 1`
+		        
 	if [ "$test" = "ReferenceError:" ]; then
 		echo "Problème avec LoadScript"
-	else
-		echo -n "Quel est le compte à partir duquel vous voulez déployer le contrat ? "
-		read account
-		echo 
-		echo -n "Veuillez entrer le mot de passe :"
-		read -s password
-		echo
-
-		#Dépoiement du contrat, nous vidons le fichier où potentielement pourrait avoir gardé du code, et nous insérons chaque ligne dans le fichier deploy.js
-		echo "" >$depl
-		ins_depl "var myAccount = web3.eth.accounts["$account"];myAccount;"
-		ins_depl "web3.personal.unlockAccount(myAccount,'$password',10000);"
-		ins_depl "var addContract = web3.eth.contract("$abi");addContract;"                                       
-		ins_depl 'var binary = "0x'$bin'";'                                                          
-		ins_depl "var estimatedGas = web3.eth.estimateGas({data: binary});estimatedGas;"                                       
-		ins_depl "var addContractInstance = addContract.new({data: binary, from: myAccount, gas: estimatedGas});addContractInstance;"
-
-		~/order.sh "loadScript('"$home$depl"');" >>$lof
-
 	fi
 fi
