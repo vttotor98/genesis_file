d4390e300f7b58edca39e7785b2be5c1c21ee157
ether <victor.tirebaque.988@gmail.com>
2018-05-25 10:43:43 +0200

    correction des paths finie

--- ether.sh
+++ ether.sh
@@ -7,22 +7,19 @@ doss=genesis_files/
 sousdoss=$doss"functions/"
 req=$doss"request/"
 
-#Création du dossier du fichier json 
+#Création du dossier de genèse 
 mkdir -p $gf
 
-cp $doss"genesis.json" $home"genesis_folder/"
+cp $doss"genesis.json" $gf
 
 #Copie du dossier contenant quelques fonctions
 cp -R $sousdoss $home
 
-tree $home
-read
-
 #Lors de l'exécution de la commande attention à l'id de la blockchain
-geth --datadir /blockchain/.ethereum_private init /blockchain/genesis_folder/genesis.json 
+geth --datadir $home".ethereum_private" init $gf"genesis.json"
 
 #Déplacement des fichiers d'exécution dans blockchain
-cp -R $req $home
+cp -R $req"*" $home
 
 echo "Exécuter start_blch.sh pour démarrer la blockchain dans $home"
 echo "Vous pouvez supprimer le dossier genesis_files"
