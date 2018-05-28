f23b14e170883e7014c287d896e07406b31ea936
vttotor98 <victor.tirebaque.988@gmail.com>
2018-05-28 10:21:47 +0200

    fusion de ether.sh et install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 #Author : Victor TIREBAQUE for Agaetis Clermont-Ferrand
 
+home=~/blockchain/
+gf=$home"genesis_folder/"
+doss=genesis_files/
+sousdoss=$doss"functions/"
+req=$doss"request/"
+
+
 if [ $1 = "ubuntu" ] ; then
 	os="sudo"
 else
@@ -23,5 +30,23 @@ fi
 
 
 #Finalisation de l'installation et initialistation de la blockchain
-genesis_files/ether.sh
+#genesis_files/ether.sh
+
+#Création du dossier de genèse 
+mkdir -p $gf
+
+cp $doss"genesis.json" $gf
+
+#Copie du dossier contenant quelques fonctions
+cp -R $sousdoss $home
+
+#Lors de l'exécution de la commande attention à l'id de la blockchain
+geth --datadir $home".ethereum_private" init $gf"genesis.json"
+
+#Déplacement des fichiers d'exécution dans blockchain
+exe="cp $req* $home"
+$exe
+
+echo "Exécuter start_blch.sh pour démarrer la blockchain dans $home"
+echo "Vous pouvez supprimer le dossier genesis_files"
 
