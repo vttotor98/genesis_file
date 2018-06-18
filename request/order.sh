c591225b24d2b1924edbe71aa1d19a148af444d0
vttotor98 <33938785+vttotor98@users.noreply.github.com>
2018-06-18 10:05:50 +0200

    Adresses modifiables

--- request/order.sh
+++ request/order.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 #Author Victor TIREBAQUE for Agaetis
 
+#Dossiers
+home=$HOME"/blockchain"
+doss=$home"/.ethereum_private/ACPrivateChain"
 csl="console"
+
+
 #Exécution d'une requête passée en argument
 if [ $1 = $csl ]; then
-  geth attach ~/blockchain/.ethereum_private/ACPrivateChain/geth.ipc $1
+  geth attach $doss/geth.ipc $1
 else
-  geth --exec $1 attach ~/blockchain/.ethereum_private/ACPrivateChain/geth.ipc
+  geth --exec $1 attach $doss/geth.ipc
 fi
